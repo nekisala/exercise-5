7f5fbd711a71be6d944b5c10a1610df184642979
Nsonsa <elombekisala@gmail.com>
2019-01-25 20:41:46 -0500

    removed the matchup method and switch statements because I was not able to continously increment the scores. Replaced switch statement with if statements and was able to keep track of scores throughout the game.

--- NsonsaKisala_Exercise5/Program.cs
+++ NsonsaKisala_Exercise5/Program.cs
@@ -10,40 +10,31 @@ namespace NsonsaKisala_Exercise5
     {
         static void Main(string[] args)
         {
-
-            bool menuIsRunning = true;
-
+            
             Console.Clear();
             Console.WriteLine("Welcome to R.P.S.L.S.");
             Console.WriteLine("**********If you don't know how to play, type \"rules\"");
             Console.WriteLine("**********If you wan't to know the score, type \"score\"");
-            
+
+            bool menuIsRunning = true;
+
+            // Keep track of scores
+            int userScore = 0;
+            int computerScore = 0;
 
             while (menuIsRunning)
             {
-                // Menu Method Call
-                Menu();
 
-                //Prompt user for input
-                string userInput = Console.ReadLine().ToLower();
 
-                //Check if input was left blank
-                while (string.IsNullOrWhiteSpace(userInput))
-                {
-                    Console.WriteLine("Can't leave this part blank. Choose one of the options from the menu.");
-                    userInput = Console.ReadLine().ToLower();
-                }
 
-                // Keep track of scores
-                //int rounds = 0;
-                int userScore = 0;
-                int computerScore = 0;
-                //int userTotal = 0;
-                //int computerTotal = 0;
+
+                // Print to console
+
+                //string results = "\nYou chose - " + userInput + "\nThe computer chose - " + optionsList[key];
 
                 
-
                 // Store list of possible options
+
                 List<string> optionsList = new List<string>();
                 optionsList.Add("rock");
                 optionsList.Add("paper");
@@ -51,61 +42,76 @@ namespace NsonsaKisala_Exercise5
                 optionsList.Add("lizard");
                 optionsList.Add("spock");
 
-                // Create Randomizer
-                Random rnd = new Random();
-                int key = rnd.Next(optionsList.Count());
-
-                int userChoiceValue = ChoiceValueNums(userInput);
-                int compChoiceValue = ChoiceValueNums(optionsList[key]);
-
-                // Print to console
-                Console.Clear();
-                Console.WriteLine("You chose - {0} \nThe computer chose - {1}", userInput, optionsList[key]);
-
-                // Print Score
-                Console.WriteLine("You : {0} \nComputer : {1}", userScore, computerScore);
-
-                // Case switch user input
-                switch (userInput)
+                while (menuIsRunning)
                 {
-                    case "rock":
-                        //rock
-                        Matchup(userInput);
-                        
-                        break;
-                    case "paper":
-                        //paper
-                        Matchup(userInput);
-                        break;
-                    case "scissors":
-                        //scissors
-                        Matchup(userInput);
-                        break;
-                    case "lizard":
-                        //lizard
-                        Matchup(userInput);
-                        break;
-                    case "spock":
-                        //spock
-                        Matchup(userInput);
-                        break;
-                    case "rules":
-                        Rules();
-                        break;
-                    case "score":
-                        
-                        break;
-                    case "exit":
-                        
-                        Console.Clear();
+                   
+                    
+                    // Menu Method Call
+                    Menu();
+                    
+                    //Prompt user for input
+                    string userInput = Console.ReadLine().ToLower();
 
+                    // Create Randomizer
+                    Random rnd = new Random();
+                    int key = rnd.Next(optionsList.Count());
+
+                    int userChoiceValue = ChoiceValueNums(userInput);
+                    int compChoiceValue = ChoiceValueNums(optionsList[key]);
+
+                    // Gather results
+                    string results = "\nYou chose - " + userInput + "\nThe computer chose - " + optionsList[key];
+
+                    //Check if input was left blank
+                    while (string.IsNullOrWhiteSpace(userInput))
+                    {
+                        Console.WriteLine("Can't leave this part blank. Choose one of the options from the menu.");
+                        userInput = Console.ReadLine().ToLower();
+                    }
+
+                    if (userInput == "rock")
+                    {
+                        Console.WriteLine(results);
+                    }
+                    else if (userInput == "paper")
+                    {
+                        Console.WriteLine(results);
+                    }
+                    else if (userInput == "scissors")
+                    {
+                        Console.WriteLine(results);
+                    }
+                    else if (userInput == "lizard")
+                    {
+                        Console.WriteLine(results);
+                    }
+                    else if (userInput == "spock")
+                    {
+                        Console.WriteLine(results);
+                    }
+                    else if (userInput == "rules")
+                    {
+                        Rules();
+                    }
+                    else if (userInput == "score")
+                    {
+                        Console.WriteLine("--------------------");
+                        //Scores(userScore, computerScore);
+                        // Print Score
+                        Console.WriteLine("You : {0} \nComputer : {1}", userScore, computerScore);
+                        Console.WriteLine("--------------------");
+                        break;
+                    }
+                    else if (userInput == "exit")
+                    {
+                        Console.Clear();
                         bool exitMenuIsRunning = true;
 
                         //Check if user really wants to exit or keep playing
                         Console.WriteLine("Done already? Are you sure? \n");
                         while (exitMenuIsRunning)
                         {
-                            
+
                             Console.Write("Type 'Y' for yes OR 'N' for no: ");
 
                             string ynInput = Console.ReadLine().ToLower();
@@ -134,59 +140,63 @@ namespace NsonsaKisala_Exercise5
                                     break;
                             }
                         }
-                        
-                        break;
-                    default:
+                    }
+                    else
+                    {
                         Console.WriteLine("Not a valid input. Choose one of the options from the menu.");
-                        break;
-                }
+                    }
 
-                
-
-
-                // Conditionals
-                if (((compChoiceValue + 1) % 5) == userChoiceValue)
-                {
-                    Console.WriteLine("You Win");
-                    userScore++;
+                    // Conditionals
+                    if (((compChoiceValue + 1) % 5) == userChoiceValue)
+                    {
+                        Console.WriteLine("You Win\n");
+                        userScore++;
+                    }
+                    else if (((compChoiceValue + 2) % 5) == userChoiceValue)
+                    {
+                        Console.WriteLine("You Win\n");
+                        userScore++;
+                    }
+                    else if (compChoiceValue == userChoiceValue)
+                    {
+                        Console.WriteLine("It's a draw \nNo points given\n");
+                    }
+                    else if (((compChoiceValue + 1) % 5) != userChoiceValue)
+                    {
+                        Console.WriteLine("You Lose\n");
+                        computerScore++;
+                    }
+                    else if (((compChoiceValue + 2) % 5) != userChoiceValue)
+                    {
+                        Console.WriteLine("You Lose\n");
+                        computerScore++;
+                    }
                 }
-                else if (((compChoiceValue + 2) % 5) == userChoiceValue)
-                {
-                    Console.WriteLine("You Win");
-                    userScore++;
-                }
-                else if (compChoiceValue == userChoiceValue)
-                {
-                    Console.WriteLine("Draw \nNo points given");
-                }
-                else
-                {
-                    Console.WriteLine("You Lose");
-                    computerScore++;
-                }
-
-                
             }
         }
 
         // METHODS --------------------------------------------------
-        // Method for the menu
+        // Method returns string menu
         public static void Menu()
         {
-            Console.WriteLine();
-            Console.WriteLine("Which do you choose: \n" +
-                    "Rock \n" +
-                    "Paper \n" +
-                    "Scissors \n" +
-                    "Lizard \n" +
-                    "Spock");
-            Console.WriteLine("-------------------------");
+            string menu = 
+                "Which do you choose: \n" +
+                "Rock \n" +
+                "Paper \n" +
+                "Scissors \n" +
+                "Lizard \n" +
+                "Spock \n-------------------------\n";
+
+            Console.WriteLine(menu);
         }
 
-        // Method for the rules
+        // Method returns string rules
         public static void Rules()
         {
-            Console.WriteLine("" +
+            Console.WriteLine("**********Reminder********** \nIf you don't know how to play, type \"rules\" anytime during the game.");
+            Console.WriteLine("If you wan't to know the score, type \"score\".\n");
+
+            string rules =
                 "Rock > Scissors \n" +
                 "Scissors > Paper \n" +
                 "Paper > Rock \n" +
@@ -196,80 +206,13 @@ namespace NsonsaKisala_Exercise5
                 "Scissors > Lizard \n" +
                 "Lizard > Paper \n" +
                 "Paper > Spock \n" +
-                "Spock > Rock \n"); 
-        }
-
-        public static void Matchup(string input)
-        {
+                "Spock > Rock \n";
             
-
-            //    // If Draw
-            //    if (input == optionsList[key])
-            //    {
-            //        Console.WriteLine("Draw \nNo points given");
-            //    }
-            //    // Rock Block --------------------------------------------------------------------------------
-            //    if ((input == "rock") && (optionsList[key] == "paper"  || optionsList[key] == "spock" ))
-            //    {
-            //        Console.WriteLine("You Lose");
-            //        computerScore++;
-            //    }
-            //    if((input == "rock") && (optionsList[key] == "lizard" || optionsList[key] == "scissors"))
-            //    {
-            //        Console.WriteLine("You Win");
-            //        userScore++;
-            //    }
-            //    // Paper Block --------------------------------------------------------------------------------
-            //    if ((input == "paper") && (optionsList[key] == "scissors" || optionsList[key] == "lizard"))
-            //    {
-            //        Console.WriteLine("You Lose");
-            //        computerScore++;
-            //    }
-            //    if ((input == "paper") && (optionsList[key] == "rock" || optionsList[key] == "spock"))
-            //    {
-            //        Console.WriteLine("You Win");
-            //        userScore++;
-            //    }
-
-            //    // Scissors Block --------------------------------------------------------------------------------
-            //    if ((input == "scissors") && (optionsList[key] == "rock" || optionsList[key] == "spock"))
-            //    {
-            //        Console.WriteLine("You Lose");
-            //        computerScore++;
-            //    }
-            //    if ((input == "scissors") && (optionsList[key] == "paper" || optionsList[key] == "lizard"))
-            //    {
-            //        Console.WriteLine("You Win");
-            //        userScore++;
-            //    }
-
-            //    //Lizard Block --------------------------------------------------------------------------------
-            //    if ((input == "lizard") && (optionsList[key] == "scissors" || optionsList[key] == "rock"))
-            //    {
-            //        Console.WriteLine("You Lose");
-            //        computerScore++;
-            //    }
-            //    if ((input == "lizard") && (optionsList[key] == "spock" || optionsList[key] == "paper"))
-            //    {
-            //        Console.WriteLine("You Win");
-            //        userScore++;
-            //    }
-
-            //    //Spock Block --------------------------------------------------------------------------------
-            //    if ((input == "spock") && (optionsList[key] == "lizard" || optionsList[key] == "paper"))
-            //    {
-            //        Console.WriteLine("You Lose");
-            //        computerScore++;
-            //    }
-            //    if ((input == "spock") && (optionsList[key] == "rock" || optionsList[key] == "scissors"))
-            //    {
-            //        Console.WriteLine("You Win");
-            //        userScore++;
-            //    }
-
-            //    PrintScore(userScore, computerScore);
+            Console.WriteLine(rules);
+            
         }
 
+        // Method returns int value foreach possible string choice
         public static int ChoiceValueNums(string choice)
         {
             if (choice == "rock")
@@ -297,26 +240,16 @@ namespace NsonsaKisala_Exercise5
                 return -1;
             }
         }
-        //public static void PrintScore(int uScore, int cScore)
-        //{
-        //    int userTotal = uScore++;
-        //    int computerTotal = cScore++;
-
-        //    Console.WriteLine();
-        //    if (uScore > cScore)
-        //    {
-        //        Console.WriteLine("You're Ahead!!!");
-        //    }
-        //    if (uScore < cScore)
-        //    {
-        //        Console.WriteLine("Uh oh, you're behind :(");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Don't stop, it's a tied game!");
-        //    }
-
-        //    Console.WriteLine("You : {0} \nComputer : {1}", userTotal, computerTotal);
-        //}
+        
+        // Method returns scores
+        public static string Scores(int uScore, int cScore)
+        {
+            string scoreboard =
+                ("-------------------------" +
+                "\nYou : " + uScore +
+                "\nComputer : "+ cScore);
+            Console.WriteLine(scoreboard);
+            return scoreboard;
+        }
     }
 }
